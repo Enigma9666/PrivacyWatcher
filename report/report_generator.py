719d614e9f8ff07b658b95637541d692570b0a80
Enigma9666 <emanuele.marino@edu-its.it>
2025-06-12 17:23:26 +0200

    Update report_generator.py

--- report/report_generator.py
+++ report/report_generator.py
@@ -10,7 +10,7 @@ def generate_txt_report(results: dict, scan_path: str, filename: str):
     """
 
     # Aggiunta: salva sempre il report nella directory 'report'
-    report_dir = os.path.join(os.getcwd(), "reports")
+    report_dir = os.path.join(os.getcwd(), "report")
     filepath = os.path.join("reports", filename)
     os.makedirs(os.path.dirname(filepath), exist_ok=True)
     # Crea la cartella 'reports' dentro 'report' se non esiste
